45af6d620a829957a239f6f210a4774eacb6c7fd
Roboat RPi <>
2023-05-06 12:20:33 -0700

    Creating new ROS Package

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'roboat_ros2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='roboat',
+    maintainer_email='roboat@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = roboat_ros2.pub:main',
+            'listener = roboat_ro2.sub:main',
+
+        ],
+    },
+)
