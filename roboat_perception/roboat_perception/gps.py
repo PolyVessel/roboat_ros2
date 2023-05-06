4d52f269459ca9a972f59bd451fa7aa48380c6ac
Dylan Starink <dylan@starink.com>
2023-05-06 14:39:14 -0700

    Add Misc Files and Comments in GPSInfo

--- roboat_perception/roboat_perception/gps.py
+++ roboat_perception/roboat_perception/gps.py
@@ -1,3 +1,16 @@
+import rclpy
+from rclpy.node import Node
+import spidev
+from ublox_gps import UbloxGps
+
+class GPS(Node):
+    def __init__(self):
+        super().__init__('gps')
+        self.port = spidev.SpiDev()
+        self.gps = UbloxGps(self.port)
+        
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+
 def main():
     print('Hi from roboat_perception.')
 
