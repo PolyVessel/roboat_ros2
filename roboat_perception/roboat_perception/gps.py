13325ec7e73f9c4539e1beb35b83718569edfba0
Dylan Starink <dylan@starink.com>
2023-05-06 15:03:01 -0700

    Add GPS Immplementation

--- roboat_perception/roboat_perception/gps.py
+++ roboat_perception/roboat_perception/gps.py
@@ -2,18 +2,47 @@ import rclpy
 from rclpy.node import Node
 import spidev
 from ublox_gps import UbloxGps
+from roboat_interfaces import GPSInfo
 
 class GPS(Node):
     def __init__(self):
         super().__init__('gps')
         self.port = spidev.SpiDev()
         self.gps = UbloxGps(self.port)
+
         
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        timer_period = 2  # seconds
+        self.timer = self.create_timer(timer_period, self.poll_gps)    
 
+        self.publisher_ = self.create_publisher(GPSInfo, '/sensor/gps_info', 10)
+    
+    def __del__(self):
+        self.port.close()
+
+    def poll_gps(self):
+        try:
+            geo = self.gps.geo_coords()
+            if geo.numSV == 0:
+                self.get_logger().info("No GPS Signal", throttle_duration_sec=60)
+
+            gps_info = GPSInfo()
+            gps_info.lon = geo.lon
+            gps_info.lat = geo.lat
+            gps_info.ground_speed = geo.gSpeed
+            gps_info.header.stamp = self.get_clock().now().to_msg();
+
+            self.publisher_.publish(gps_info)
+        except (ValueError, IOError) as err:
+            self.get_logger().error(f"GPS Error! {err}", throttle_duration_sec=60)
+        
 def main():
-    print('Hi from roboat_perception.')
+    gps_node = GPS()
 
+    rclpy.spin(gps_node)
+
+    gps_node.destroy_node()
+
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
