b055b8f1fbc5bd214f34a1bdfb70e709382a6b3c
Dylan Starink <dylan@starink.com>
2023-05-08 00:44:43 +0000

    Change GPS to use std_msgs instead of custom msg

--- roboat_perception/roboat_perception/gps.py
+++ roboat_perception/roboat_perception/gps.py
@@ -1,8 +1,9 @@
 import rclpy
 from rclpy.node import Node
 import spidev
-from ublox_gps import UbloxGps
-from roboat_interfaces.msg import GPSInfo
+from ublox_gps import UbloxGps, core, sparkfun_predefines
+from sensor_msgs.msg import NavSatFix
+from std_msgs.msg import Header
 import subprocess
 import os
 
@@ -19,7 +20,7 @@ class GPS(Node):
         timer_period = 60  # seconds
         self.gps_time_timer = self.create_timer(timer_period, self.set_time)    
 
-        self.publisher_ = self.create_publisher(GPSInfo, '/sensor/gps_info', 10)
+        self.publisher_ = self.create_publisher(NavSatFix, '/sensor/gpsfix', 10)
         
         self.get_logger().info("GPS Initialized")
     
@@ -29,16 +30,35 @@ class GPS(Node):
     def poll_gps(self):
         try:
             geo = self.gps.geo_coords()
+            cov = self._get_cov_gps();
+
             if geo.numSV == 0:
                 self.get_logger().info("No GPS Signal", throttle_duration_sec=60)
 
-            gps_info = GPSInfo()
-            gps_info.lon = geo.lon
-            gps_info.lat = geo.lat
-            gps_info.ground_speed = geo.gSpeed
-            gps_info.header.stamp = self.get_clock().now().to_msg();
+            # From https://github.com/FrankBu0616/ros2_zed_f9r_gps/blob/main/zed_f9r_gps/zed_f9r_gps/publish_gps_data.py
+            # and https://github.com/sparkfun/Qwiic_Ublox_Gps_Py/pull/26
+            msg = NavSatFix()
 
-            self.publisher_.publish(gps_info)
+            msg.header = Header()
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.header.frame_id = "gps"
+
+            msg.longitude = geo.lon
+            msg.latitude = geo.lat
+            msg.altitude = 0.001 * float(geo.height)
+            
+            NN = cov.posCovNN
+            NE = cov.posCovNE
+            ND = cov.posCovND
+            EE = cov.posCovEE
+            ED = cov.posCovED
+            DD = cov.posCovDD
+            # Following the convention here: https://www.ros.org/reps/rep-0105.html
+            msg.position_covariance = [EE, NE, ED, NE, NN, ND, ED, ND, DD]
+            msg.position_covariance_type = 3
+
+            self.publisher_.publish(msg)
+
             self.get_logger().info(f"Successfully published first gps_coords: {geo.lat}, {geo.lon} at {geo.gSpeed} mm/s", once=True)
         except (ValueError, IOError) as err:
             self.get_logger().error(f"GPS Error! {err}", throttle_duration_sec=60)
@@ -59,6 +79,21 @@ class GPS(Node):
             self.get_logger().info(f"First Set Time to {gps_utc} UTC", once=True)
         except Exception as e:
             self.get_logger().error(f"GPS Error when setting time! {e}", throttle_duration_sec=60)
+    
+    def _get_cov_gps(self):
+        """
+        Sends a poll request for the NAV class with the COV Message ID and
+        parses ublox messages for the response. The payload is extracted from
+        the response which is then passed to the user.
+
+        :return: The payload of the NAV Class and COV Message ID
+        :rtype: namedtuple
+        """
+        self.gps.send_message(sparkfun_predefines.NAV_CLS, self.gps.nav_ms.get('COV'))
+        parse_tool = core.Parser([sparkfun_predefines.NAV_CLS])
+        cls_name, msg_name, payload = parse_tool.receive_from(self.gps.hard_port)
+
+        return payload
 
         
 def main(args=None):
