f6cc9a74a0fb014ca4dd9f478c8fc51238723dca
Pranav Yerramilli <satyapranav.y@gmail.com>
2023-11-04 20:13:58 +0000

    Workday 11/4/23

--- src/roboat_perception/roboat_perception/dtp.py
+++ src/roboat_perception/roboat_perception/dtp.py
@@ -1,5 +1,29 @@
-def hash_fnv1a_32(data: bytes):
+from bitstring import Bits, BitArray
+def hash_fnv1a_32(data: Bits) -> Bits:
+    # pad w/ 0s after subtracting length of smaller 
     hash = 0x811c9dc5
-    for x in data:
-        hash = ((ord(x) ^ hash) * 0x01000193) & 0xFFFFFFFF
-    return hash
+    for x in data.cut(8):
+        hash = (((hash ^ x.uintbe) * 0x01000193)) % (2 ** 32)
+        
+    return Bits(uintbe=hash, length=32)
+
+# packet num & total packet num is 1,
+# create bits object for pck num, 16 bits
+# add data, add checksum
+def create_packet(data: Bits) -> Bits:
+    result = BitArray('data')
+    result.append()
+    return result
+    # calculate size
+
+check = create_packet(b'abc123')
+print(check)
+# 
+# def change_to_bits(data: Bits) -> Bits:
+#     # Get the hashing output
+#     store = hash_fnv1a_32(data)
+#     # Convert to bits
+#     txt = Bits(store)
+#     output = txt.__str__
+
+# .
--- src/roboat_perception/test/test_fnv1a.py
+++ src/roboat_perception/test/test_fnv1a.py
@@ -1,6 +1,10 @@
 from roboat_perception.dtp import hash_fnv1a_32
+from bitstring import Bits
 
 def test_fnv1a_32_calc():
-    b1 = b'abc123'
+    b1 = Bits(b'abc123')
     # Use bitstring here
-    assert hash_fnv1a_32(b1) == '38b29a05' 
+    assert hash_fnv1a_32(b1) == Bits('0x38b29a05')
+
+# def test_change_to_bits():
+#     pass
