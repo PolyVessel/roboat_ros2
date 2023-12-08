4ec64dd41a9d08548cf256448319162be218963b
Pranav Yerramilli <satyapranav.y@gmail.com>
2023-12-08 18:15:13 +0000

    Added dataclass to dtp.py and changed test file

--- src/roboat_perception/roboat_perception/dtp.py
+++ src/roboat_perception/roboat_perception/dtp.py
@@ -1,36 +1,43 @@
 from bitstring import Bits, BitArray
-def hash_fnv1a_32(data: Bits) -> Bits:
-    # pad w/ 0s after subtracting length of smaller 
-    hash = 0x811c9dc5
-    for x in data.cut(8):
-        hash = (((hash ^ x.uintbe) * 0x01000193)) % (2 ** 32)
-        
-    return Bits(uintbe=hash, length=32)
+from dataclasses import dataclass
+@dataclass
+class DTP:
+    data: Bits
+    
+    @staticmethod
+    def hash_fnv1a_32(data: Bits) -> Bits:
+        # pad w/ 0s after subtracting length of smaller 
+        hash = 0x811c9dc5
+        for x in data.cut(8):
+            hash = (((hash ^ x.uintbe) * 0x01000193)) % (2 ** 32)
+            
+        return Bits(uintbe=hash, length=32)
 
-# packet num & total packet num is 1,
-# create bits object for pck num, 16 bits
-# add data, add checksum
-def create_packet(data: Bits) -> BitArray:
-    store = BitArray(data)
-    check_sum = hash_fnv1a_32(data)
-    # adding packet num
-    store.prepend(BitArray(1))
-    # adding hashed data to be sent
-    store.append(check_sum[0:16])
-    # output is full message
-    return store
-    # calculate size
+    # packet num & total packet num is 1,
+    # create bits object for pck num, 16 bits
+    # add data, add checksum
+    def create_packet(data: Bits) -> BitArray:
+        store = BitArray(data)
+        check_sum = self.hash_fnv1a_32(data)
+        # adding packet num
+        store.prepend(BitArray(1))
+        # adding hashed data to be sent
+        store.append(check_sum[0:16])
+        # output is full message
+        return store
+        # calculate size
 
-# takes bits, checks proper size, checks if the hash is accurate
-def decode_packet(data: BitArray) -> Bits:
-    # Store part of message to compare
-    checker = data[-16:]
-    # Store message
-    data2 = data[32:-16]
-    # Store part of message to hash & compare with
-    checksum = hash_fnv1a_32(data2[8:])
-    # Compare
-    if checksum[0:16] != checker:
-        return None
-    # Returning message
-    return data[16:32] + data2[8:]
+    # takes bits, checks proper size, checks if the hash is accurate
+    def decode_packet(data: BitArray) -> Bits:
+        # Store part of message to compare
+        checker = data[-16:]
+        # Store message
+        data2 = data[32:-16]
+        # Store part of message to hash & compare with
+        checksum = self.hash_fnv1a_32(data2)
+        # Compare
+        if checksum[0:16] != checker:
+            return None
+        # Returning message
+        self.data = Bits(data[16:32] + data2)
+        return self.data
--- src/roboat_perception/test/test_fnv1a.py
+++ src/roboat_perception/test/test_fnv1a.py
@@ -1,7 +1,7 @@
-from roboat_perception.dtp import hash_fnv1a_32
+from roboat_perception.dtp import DTP
 from bitstring import Bits
 
 def test_fnv1a_32_calc():
-    b1 = Bits(b'abc123')
+    dtp = DTP(Bits(b'abc123'))
     # Use bitstring here
-    assert hash_fnv1a_32(b1) == Bits('0x38b29a05')
+    assert dtp.hash_fnv1a_32(dtp.data) == Bits('0x38b29a05')
