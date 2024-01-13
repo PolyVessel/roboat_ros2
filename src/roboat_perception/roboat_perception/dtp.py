972af9aa7792234677aa44386880c04f35f6b81c
Pranav Yerramilli <satyapranav.y@gmail.com>
2024-01-13 21:44:07 +0000

    Added test cases, changed decode_packet to output as an object

--- src/roboat_perception/roboat_perception/dtp.py
+++ src/roboat_perception/roboat_perception/dtp.py
@@ -1,43 +1,52 @@
 from bitstring import Bits, BitArray
 from dataclasses import dataclass
+
 @dataclass
-class DTP:
+class DecodedPacket:
     data: Bits
+    packet_num: Bits
+    total_packet_num: Bits
     
-    @staticmethod
-    def hash_fnv1a_32(data: Bits) -> Bits:
-        # pad w/ 0s after subtracting length of smaller 
-        hash = 0x811c9dc5
-        for x in data.cut(8):
-            hash = (((hash ^ x.uintbe) * 0x01000193)) % (2 ** 32)
-            
-        return Bits(uintbe=hash, length=32)
+def hash_fnv1a_32(data: Bits) -> Bits:
+    # pad w/ 0s after subtracting length of smaller 
+    hash = 0x811c9dc5
+    for x in data.cut(8):
+        hash = (((hash ^ x.uintbe) * 0x01000193)) % (2 ** 32)
+    return Bits(uintbe=hash, length=32)
 
-    # packet num & total packet num is 1,
-    # create bits object for pck num, 16 bits
-    # add data, add checksum
-    def create_packet(data: Bits) -> BitArray:
-        store = BitArray(data)
-        check_sum = self.hash_fnv1a_32(data)
-        # adding packet num
-        store.prepend(BitArray(1))
-        # adding hashed data to be sent
-        store.append(check_sum[0:16])
-        # output is full message
-        return store
-        # calculate size
+# packet num & total packet num is 1, create bits object for pck num, 16 bits
+# add data, add checksum
+def create_packet(data: Bits) -> BitArray:
+    store = BitArray(data)
+    check_sum = hash_fnv1a_32(data)
+    print(f"check_sum: {check_sum}")
+    
+    # Total Pkt Num is 1
+    store.prepend(BitArray(uint=1, length=16))
+    
+    # adding packet num
+    store.prepend(BitArray(uint=1, length=16))
+    
+    # adding hashed data to be sent
+    store.append(check_sum[0:16])
+    # outputting the full message
+    print(f"bitarray: {store}")
+    return store
 
-    # takes bits, checks proper size, checks if the hash is accurate
-    def decode_packet(data: BitArray) -> Bits:
-        # Store part of message to compare
-        checker = data[-16:]
-        # Store message
-        data2 = data[32:-16]
-        # Store part of message to hash & compare with
-        checksum = self.hash_fnv1a_32(data2)
-        # Compare
-        if checksum[0:16] != checker:
-            return None
-        # Returning message
-        self.data = Bits(data[16:32] + data2)
-        return self.data
+# takes bits, checks proper size, checks if the hash is accurate
+def decode_packet(packet: BitArray) ->  DecodedPacket | None:
+    # Store part of message to compare
+    checker = packet[-16:]
+    # Store message
+    message = packet[32:-16]
+    print(f"data2: {message}")
+    # Store part of message to hash & compare with
+    checksum = hash_fnv1a_32(message)
+    # Compare
+    print(f"check: {checker}")
+    print(f"checksum: {checksum}")
+    if checksum[0:16] != checker:
+        return None
+    # Returning message
+    output = DecodedPacket(Bits(message), packet[0:16], packet[16:32])
+    return output
--- src/roboat_perception/test/test_fnv1a.py
+++ src/roboat_perception/test/test_fnv1a.py
@@ -1,7 +1,34 @@
-from roboat_perception.dtp import DTP
-from bitstring import Bits
+from roboat_perception.dtp import DecodedPacket
+from roboat_perception.dtp import hash_fnv1a_32
+from roboat_perception.dtp import create_packet
+from roboat_perception.dtp import decode_packet
+from bitstring import Bits, BitArray
+import unittest
 
-def test_fnv1a_32_calc():
-    dtp = DTP(Bits(b'abc123'))
-    # Use bitstring here
-    assert dtp.hash_fnv1a_32(dtp.data) == Bits('0x38b29a05')
+class TestDTP(unittest.TestCase):
+    def test_fnv1a_32_calc(self):
+        assert hash_fnv1a_32(Bits(b'abc123')) == Bits('0x38b29a05')
+
+    def test_create_packet(self):
+        data = Bits(b'abc123')
+        store = create_packet(data)
+        expected = BitArray('0x0001000161626331323338b2')
+        self.assertEqual(store, expected)
+
+    def test_decode_packet_valid(self):
+        data = Bits(b'abc123')
+        store = create_packet(data)
+        output = decode_packet(store)
+        print(f"method output: {output}")
+        print(Bits(b'abc123'))
+        self.assertEqual(output, DecodedPacket(Bits(b'abc123'), BitArray('0x0001'), BitArray('0x0001')))
+    
+    def test_decode_packet_invalid(self):
+        data = Bits(b'ac123')
+        encoded = create_packet(data)
+        del encoded[36:44]
+        output = decode_packet(encoded)
+        self.assertIsNone(output)
+        
+if __name__ == '__main__':
+    unittest.main()
