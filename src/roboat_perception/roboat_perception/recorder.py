a09f3c748a1318e1d6ddb98632ec4c0da94b356d
Dylan Starink <dylan@starink.com>
2024-05-04 02:28:15 +0000

    Update protos and use Sqlite DB

--- src/roboat_perception/roboat_perception/recorder.py
+++ src/roboat_perception/roboat_perception/recorder.py
@@ -1,81 +1,43 @@
 from datetime import datetime
-import uuid
-import serial
-import RPi.GPIO as GPIO
 from time import sleep
-from typing import List, NamedTuple
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
-import subprocess
 import os
 from sensor_msgs.msg import NavSatFix
 from csv import writer
 import os as os
-import uuid
 from glob import glob 
+import sqlite3
 
 
 class Recorder(Node):
-    def __init__(self, directory):
+    def __init__(self, path):
         super().__init__('recorder')
         self.subscription = self.create_subscription(NavSatFix, '/sensor/gpsfix', self.gps_callback, 10)
-        self.directory = directory
-        self.filename = self.generate_unique_filename()
-        self.file_path = os.path.join(self.directory, self.filename)
+        self.con = sqlite3.connect(path)
+        cur = self.con.cursor()
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS  "GPS" (
+                "Lat" REAL NOT NULL,
+                "Lon" REAL NOT NULL,
+                "Timestamp" INTEGER NOT NULL
+            ) STRICT;
+        """)
+        self.con.commit()
+        
+        self.get_logger().info("Recorder successfully initialized!")
 
     def gps_callback(self, getMsg: NavSatFix):
-        coordinates = (getMsg.latitude, getMsg.longitude)
-        timestamp = getMsg.header.stamp
+        print(getMsg.latitude, getMsg.longitude, getMsg.header.stamp.sec)
         
+        self.con.execute("INSERT INTO GPS(Lat, Lon, Timestamp) VALUES (?, ?, ?)",
+            (getMsg.latitude, getMsg.longitude, getMsg.header.stamp.sec))
+        self.con.commit()
 
-        # Check file size before writing
-        if os.path.exists(self.file_path) and os.path.getsize(self.file_path) > 5_000_000:
-            # Create a new file if the size exceeds the limit
-            self.filename = self.generate_unique_filename()
-            self.file_path = os.path.join(self.directory, self.filename)
-
-        with open(os.path.join(self.directory, self.filename), 'a+') as collect_info:
-            writer_object = writer(collect_info)
-            writer_object.writerow([timestamp.sec, coordinates[0], coordinates[1]])
-            
-        self.delete_oldest_file()
-
-        # if os.stat('~/gps_log.csv').st_size > 5_000_000:  
-        #      f = open("newinfo.txt", "w")
-            # f.write(f" {timestamp}, {coordinates[0]}, {coordinates[1]}\n")
-            
-        
-        
-
-    def generate_unique_filename(self):
-        # unique_id = str(uuid.uuid4())
-        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-        filename = f"{timestamp}.csv"
-        return filename
-                
-   
-    def delete_oldest_file(self):
-        # Get a list of files matching the naming pattern in the specified directory
-        files = glob(os.path.join(self.directory, '*.csv'))
-
-        # Sort files based on creation time
-        sorted_files = sorted(files, key=lambda x: os.path.getctime(x))
-
-        # Delete the oldest file if exceeding a certain condition (e.g., more than 5 files)
-        while len(sorted_files) > 5:
-            file_to_delete = sorted_files.pop(0)
-            os.remove(file_to_delete)
-
-        
-        
-        
-        
-        
         
 def main():
     rclpy.init()
-    node = Recorder('/home/roboat/logs')
+    node = Recorder('/tmp/recording.db') # TODO: Change me so we persist data, this is to protect SD card
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
