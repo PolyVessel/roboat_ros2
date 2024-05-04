beeeec886d788fde130b7c78f2fa1973611d5c59
Dylan Starink <dylan@starink.com>
2024-05-04 17:39:14 +0000

    Create thinking pkg and move recorder to it
    Co-authored-by: rafpug <rafpug@users.noreply.github.com>

--- src/roboat_perception/setup.py
+++ src/roboat_perception/setup.py
@@ -5,44 +5,12 @@ from subprocess import check_call
 import os
 import sys
 
-
-def compile_protos():
-    protos_dir = "../../protos"
-    generated_files_dir = "./roboat_perception/pb"
-    
-    check_call(f"rm -f {generated_files_dir}/*_pb2.py", shell=True)
-    
-    files_in_protos_dir = os.listdir(protos_dir)
-    
-    proto_files = [file for file in files_in_protos_dir if file.endswith('.proto')]
-    
-    if len(proto_files) == 0:
-        print("No Protobuf Files found to compile")
-        return
-    
-    check_call(f"protoc --python_out={generated_files_dir} \
-                       --proto_path {protos_dir} \
-                       --experimental_allow_proto3_optional \
-                       {protos_dir}/*.proto", shell=True)
-
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
-    def run(self):
-        compile_protos()
-        develop.run(self)
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        compile_protos()
-        install.run(self)
-
 package_name = 'roboat_perception'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name, f"{package_name}/pb", f"{package_name}/data"],
+    packages=[package_name, f"{package_name}/data"],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -61,9 +29,5 @@ setup(
             'radio = roboat_perception.radio:main',
             'recorder = roboat_perception.recorder:main'
         ],
-    },
-    cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
-    },
+    }
 )
---
+++ src/roboat_thinking/README.md
@@ -0,0 +1,14 @@
+# Roboat Perception Package
+All nodes related to gathering information from the environment (GPS, Radio, etc.)
+
+#### Directories
+- `protos` - Location for Protocol Buffer .proto files.
+- `resource` - Marker file for ROS2 to tell that this is a package.
+- `roboat_perception` - Main location for python nodes + libraries.
+- `test` - Location for tests for this package
+
+#### Loose Files
+- `LICENSE` - General License file for package
+- `package.xml` - Declare dependencies and other misc. package info.
+- `setup.cfg`- Metadata for setup.py
+- `setup.py` - Script for building + setting up package with setuptools.
