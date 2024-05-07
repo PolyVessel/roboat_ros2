7d675c940137b855ad9d4be05df43f6fb768aaf4
Dylan Starink <dylan@starink.com>
2024-05-07 03:20:18 +0000

    Add enabled publisher

--- src/roboat_thinking/roboat_thinking/msg_decoder.py
+++ src/roboat_thinking/roboat_thinking/msg_decoder.py
@@ -3,6 +3,7 @@ from typing import List, NamedTuple
 import rclpy
 from rclpy.node import Node
 from roboat_interfaces.msg import RawData
+from std_msgs.msg import Bool
 import subprocess
 import os
 from bitstring import Bits, BitArray
@@ -13,8 +14,10 @@ class MessageDecoder(Node):
     def __init__(self):
         super().__init__('radio')
      
-        self.publisher_ = self.create_publisher(RawData, '/radio/raw_send', 10)
+        self.raw_radio_publisher = self.create_publisher(RawData, '/radio/raw_send', 10)
         self.subscription_ = self.create_subscription(RawData, '/radio/raw_recv', self.decode, 10)
+
+        self.enabled_publisher = self.create_publisher(Bool, '/signals/enabled', 10)
 
         # self.timer = self.create_timer(timer_period_sec, self.read_from_radio) # Temperary Timer
     
@@ -37,7 +40,7 @@ class MessageDecoder(Node):
         msg_to_send = ToShoreResponse()
         msg_to_send.response.echo.SetInParent()
         encoded_bytes = msg_to_send.SerializeToString()
-        self.publisher_.publish([encoded_bytes[i].to_bytes(1, 'big') for i in range(len(encoded_bytes))])
+        self.raw_radio_publisher.publish([encoded_bytes[i].to_bytes(1, 'big') for i in range(len(encoded_bytes))])
         
 
 def main(args=None):
